b794c9becfc5c03b2b1794389f4d5d7650e32bc4
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2020-03-31 11:06:32 -0300

    Sort file sistem.

---
+++ src/scenes/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Home() {
+  return (
+    <div className="App">
+      <h1>Articles</h1>
+
+      {/* <ArticlesList/> */}
+    </div>
+  );
+}
+
+export default Home;
